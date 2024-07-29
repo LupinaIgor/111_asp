a7b536b3089b4190a284353981ef7c8d2dd96c6c
LupinaIgor <igorlupina222@gmail.com>
2024-07-29 17:31:38 +0300

    Two pages

---
+++ MyFirstWebServer222/MyFirstWebServer222/Data/Migrations/20240729122126_CreateSubscribeModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFirstWebServer222.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSubscribeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Subscribers",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Subscribers",
+                newName: "id");
+        }
+    }
+}
